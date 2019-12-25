0ef152cdf6ad2ef9b634808c4c541e8e62823ce6
lehuyhoang999 <57885429+lehuyhoang999@users.noreply.github.com>
2019-12-25 10:56:53 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -24,7 +24,24 @@ firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
-const configuration = {iceServers: [{urls: 'stun:stun.l.google.com:19302'}]};
+const configuration = {iceServers: [{urls: ['stun:stun.l.google.com:19302',
+'stun:stun.l.google.com:19302',
+'stun:stun1.l.google.com:19302',
+'stun:stun2.l.google.com:19302',
+'stun:stun3.l.google.com:19302',
+'stun:stun4.l.google.com:19302',
+'stun:stun.ekiga.net',
+'stun:stun.ideasip.com',
+'stun:stun.rixtelecom.se',
+'stun:stun.schlund.de',
+'stun:stun.stunprotocol.org:3478',
+'stun:stun.voiparound.com',
+'stun:stun.voipbuster.com',
+'stun:stun.voipstunt.com',
+'stun:stun.voxgratia.org',
+'turn:turn01.hubl.in?transport=udp',
+'turn:turn02.hubl.in?transport=tcp'
+]}]};
 
 const remoteView = document.getElementById('remoteStream');
 const config = {audio: true, video: true};
